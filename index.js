f92a6a28e8b590757218b494691ca52b9217cf7f
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2025-02-05 19:33:39 +0530

    added JS functions, if/else, loops and concept of lexical scoping

--- index.js
+++ index.js
@@ -93,3 +93,77 @@ console.log(add(55,100,45));
 // 9. Nullish Coalescing (??): Returns the right-hand value if the left-hand value is null or undefined.
 let username = null;
 console.log(username ?? "Guest");
+
+// Functions: They helps to implement a particular function. They help in code reusability.
+// Function declaration
+function greet(name) {
+    return "Hello, " + name;
+}
+// Function call
+let message = greet("John"); // "Hello, John"
+//function to multiply n numbers
+function multiplyNumber(...numbers){
+    let result = 1;
+    for(let i=0; i<numbers.length; i++){
+        result *= numbers[i];
+    }
+    return result;
+}
+console.log(multiplyNumber(1,2,3,4,5,6,7,8,9,10));
+//function to tell if a person can vote or not.
+function Vote(age){
+    return age>=18? true:false;
+}
+console.log(Vote(25));
+
+// if-else: these are conditional statements that is, they implement the block of code if the condition is true.
+let num = 25;
+if(num<10){
+    console.log("The number is less than 10");
+}
+else if(num<20){
+    console.log("The number is less than 20");
+}
+else{
+    console.log("The number is greater than 20");
+}
+//check for odd or even
+if(num%2 === 0){
+    console.log("Number is even");
+}
+else{
+    console.log("Number is odd");
+}
+
+//Loops: They are used to repeat a block of code; they are of 2 types:
+// 1. for loop: used to repeat a block of code given number of times. Here the example of a program that display first 5 whole numbers
+for(let i=0; i<5; i++){
+    console.log(i);
+}
+// 2. while loop: thay are used to repeat a block of code until a given condition is false.
+let flag = 0;
+while(flag<5){
+    console.log(flag);
+    flag++;
+}
+
+// concept of functional scope and block scope
+// functional scope: the variables declared with var are functional scoped. The variable is only accessible within the function in which it is declared. Eg:
+function example1(){
+    var x = 1;
+    if(true){
+        var x = 2; //redeclared the same variable
+    } 
+    console.log(x);
+}
+example1();
+// block scope: variable declared with let and const are block scoped. The variable is only accessible within the curly brace in ehich it is declared. each bllock creates a new scope. Eg:
+function example2(){
+    let x = 1;
+    if(true){
+        let x = 2; // it treats as a different variable. x is a local variable here. if i dont declare this variable and try to log x, then it print 1 as the outer x acts as a global variable and it is accessible inside this if block.
+        // This is the concept of Lexical Scoping
+    }
+    console.log(x);// prints 1
+}
+example2();
